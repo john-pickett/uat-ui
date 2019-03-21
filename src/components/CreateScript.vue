aa18e5a9d94a21789a523e8bbc6d0a2fb6e0c579
John Pickett <john.pickett@maryville.com>
2019-03-21 08:59:09 -0500

    Initial commit

---
+++ src/components/CreateScript.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-container>
+		<v-layout text-xs-center wrap>
+			<v-flex mb-4>
+				<h1 class="display-2 font-weight-bold mb-3">Create Script</h1>
+			</v-flex>
+			<v-flex mb-5 xs12>
+				<p>Content here</p>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      
+    })
+  }
+</script>
---
+++ src/components/ViewScript.vue
@@ -0,0 +1,20 @@
+<template>
+	<v-container>
+		<v-layout text-xs-center wrap>
+			<v-flex mb-4>
+				<h1 class="display-2 font-weight-bold mb-3">View Script</h1>
+			</v-flex>
+			<v-flex mb-5 xs12>
+				<p>content here</p>
+			</v-flex>
+		</v-layout>
+	</v-container>
+</template>
+
+<script>
+  export default {
+    data: () => ({
+      
+    })
+  }
+</script>
